87c7ef55db50171e32a43da7063a0df0bbd9bdbf
BILAL ALI <147367360+bilalali475675@users.noreply.github.com>
2024-04-11 08:10:02 +0500

    Add files via upload

---
+++ caluclater.py.py
@@ -0,0 +1,11 @@
+print("Hello Sir")
+
+x = int(input("Enter a first number: "))
+y = int(input("Enter a second number: "))
+
+print("The sum is",x+y)
+print("The substration is",x-y)
+print("The multiplication is",x*y)
+print("The division is",x/y)
+print("The modulus is",x%y)
+print("The power/exponential is",x**y)
